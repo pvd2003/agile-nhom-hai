a92977f45ca13c262a2c28511041dd8f50d7d015
HoangMylb <nguyenmylb85@gmail.com>
2023-05-26 21:15:22 +0700

    HiHi

--- components/app/product/screens/Detail.js
+++ components/app/product/screens/Detail.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, Image,Button, Pressable , ScrollView } from 'react-native'
 import React from 'react'
-
+ // Hoang My ne ahii
 const Detail = () => {
   return (
     <ScrollView style={styles.container}>
