8bdeccb4d66f4200cefd05c4a9df57cdd361e985
pvd2003 <ducpvps21704@fpt.edu.vn>
2023-06-08 00:18:32 +0700

    sap-xep-lai-project

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import React from 'react'
 import AppNavigation from './components/app/navigation/AppNavigation'
 import Detail from './components/app/product/screens/Detail'
 import Home from './components/app/product/screens/Home'
-import Login from './components/app/product/screens/Login'
+import Login from './components/app/user/screens/Login'
 import Boarding from './components/app/product/screens/Boarding'
-import Signup from './components/app/product/screens/Signup'
+import Signup from './components/app/user/screens/Signup'
 
 const App = () => {
   return (
