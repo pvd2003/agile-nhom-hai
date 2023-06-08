a5b5ae9409d86479449355cd94386a22e6f3267a
HoangMylb <nguyenmylb85@gmail.com>
2023-06-08 19:20:25 +0700

    Payment Card

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import AppNavigation from './components/app/navigation/AppNavigation'
 import Detail from './components/app/product/screens/Detail'
+import Payment from './components/app/product/screens/Payment'
 
+Payment
 const App = () => {
   return (
-    <Detail/>
+    <Payment/>
   )
 }
 
