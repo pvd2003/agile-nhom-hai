77f38fd7b3516364ec27dd9492174e7d7c39bead
khoicr <118146764+khoicr@users.noreply.github.com>
2023-06-09 22:47:16 +0700

    done Addpayment,Review,Setting

--- App.js
+++ App.js
@@ -5,14 +5,20 @@ import Detail from './components/app/product/screens/Detail'
 import Home from './components/app/product/screens/Home'
 import Login from './components/app/user/screens/Login'
 import Boarding from './components/app/user/screens/Boarding'
+import Review from './components/app/product/screens/Review'
+import Seting from './components/app/product/screens/Seting'
+import AddPayment from './components/app/product/screens/AddPayment'
 
 const App = () => {
   return (
-    // <Signup/>
+    //<Signup/>
     // <Boarding/>
     // <Login/>
     // <Detail/>
-    <AppNavigation/>
+    //<AppNavigation/>
+   //<Review/>
+   <Seting/>
+   //<AddPayment/>
   )
 }
 
